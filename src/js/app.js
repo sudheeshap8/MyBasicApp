f313d341e49ad6a77aa6dd236ee2a8b610b5c9e2
Sudheesh Parambil <hellosudhee@gmail.com>
2021-06-13 20:59:47 +0400

    Update available notification by serviceworker

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-console.log('app.js');
+console.log('ABC...');
+
+window.isUpdateAvailable.then(isAvailable => {
+
+  const elem = document.getElementById('update-status');
+		
+  if (isAvailable) {
+
+    elem.textContent = 'YES';
+
+    return;
+	}
+
+  elem.textContent = 'NO';
+});
